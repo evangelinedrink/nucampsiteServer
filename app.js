1f81420193021368b2cdc65a2707d3f6401ee2d5
Evangeline Drink <evangelinedrink@gmail.com>
2021-05-26 12:13:18 -0700

    Basic Authentication

--- app.js
+++ app.js
@@ -38,10 +38,41 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+//In this block of code below is our Middleware functions. They run in the position they are in the code (in sequence).
+app.use(logger('dev')); //Morgan Logger: logs information in the console
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//Authentification Middleware will be placed before the Express.static() middleware so that users have to authenticate their credentials before accessing the Express server.
+//auth function, like all Express middleware functions, has the req, res, next (optional) paremeter
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader= req.headers.authorization;
+  if(!authHeader) {//If authHeader is null, this means the user has not placed a username or password in.
+    const err= new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic"); //This lets the client know that the server is requesting authentication and the authentication method being requested is Basic 
+    err.status= 401; //Error code when authentication is not given
+    return next(err); //Server will send the error message back and ask for authentication from the client
+  }
+  //If the client then gives their username and password, an authorization header will then be submitted to the server
+  //Buffer global class in Node is one of the few classes in Node that we can just use.
+  //Buffer.from() just decodes the username and password from Base-64 encoded string
+  //Argument inside of Buffer.from: It takes the authorization header and extract the username and password from it. Then it places it in the auth array as username and password.
+  const auth= Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+  const user= auth[0]; //Getting the client's username from the auth array
+  const pass = auth[1]; //Getting the client's password from the auth array
+  
+  //Basic Validation 
+  if(user ==="admin" && pass ==="password") {
+    return next(); //The user is authorized to use the server
+  } else { //An error shows if the user doesn't type admin and password for the username and password
+      const err = new Error("You are not authenticated!"); //Goes to the Express error handler
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status= 401;
+      return next(err);
+  }
+}
+app.use(auth); //Lets the auth middleware function run before the Express.static() middleware function. auth function makes sure that the user has inputted their credentials (authetnication)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
