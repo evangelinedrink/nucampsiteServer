e9ad7cf5a5e587eb4001ade43edcb63d94ae6cd8
Evangeline Drink <evangelinedrink@gmail.com>
2021-05-28 20:46:16 -0700

    Cookies

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser'); //Middleware of Cookie-Parser
 var logger = require('morgan'); //Morgan Middleware shows what is happening in the console in the BASH Terminal (like if there is a POST, GET, etc.)
 
 var indexRouter = require('./routes/index');
@@ -42,34 +42,51 @@ app.set('view engine', 'jade');
 app.use(logger('dev')); //Morgan Logger: logs information in the console
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("12345-67890-09876-54321")); //Cookie Parser is set up here to be used.  The secret key for the cookie parser is placed inside of the parenthesis. Secret key doesn't have to be meaningful, it is a key that is sent to the cookie parser that will encrypt the information.
 //Authentification Middleware will be placed before the Express.static() middleware so that users have to authenticate their credentials before accessing the Express server.
 //auth function, like all Express middleware functions, has the req, res, next (optional) paremeter
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader= req.headers.authorization;
-  if(!authHeader) {//If authHeader is null, this means the user has not placed a username or password in.
-    const err= new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic"); //This lets the client know that the server is requesting authentication and the authentication method being requested is Basic 
-    err.status= 401; //Error code when authentication is not given
-    return next(err); //Server will send the error message back and ask for authentication from the client
-  }
-  //If the client then gives their username and password, an authorization header will then be submitted to the server
-  //Buffer global class in Node is one of the few classes in Node that we can just use.
-  //Buffer.from() just decodes the username and password from Base-64 encoded string
-  //Argument inside of Buffer.from: It takes the authorization header and extract the username and password from it. Then it places it in the auth array as username and password.
-  const auth= Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-  const user= auth[0]; //Getting the client's username from the auth array
-  const pass = auth[1]; //Getting the client's password from the auth array
-  
-  //Basic Validation 
-  if(user ==="admin" && pass ==="password") {
-    return next(); //The user is authorized to use the server
-  } else { //An error shows if the user doesn't type admin and password for the username and password
-      const err = new Error("You are not authenticated!"); //Goes to the Express error handler
-      res.setHeader("WWW-Authenticate", "Basic");
-      err.status= 401;
-      return next(err);
+  //console.log(req.headers); //This console logs the request header
+  //If request does not contained the signed cookie nor its value, it will be parsed as false. This means the user has not authenticated their username and password.
+  if(!req.signedCookies.user) { //signedCookies property of the request object is provided by the cookie parser. It will automatically parse a signed cookie from the request. If the cookie is not properly signed, it will return a value of False. The user property is something that will be added to the signed cookie.
+    const authHeader= req.headers.authorization;
+    if(!authHeader) {//If authHeader is null, this means the user has not placed a username or password in.
+      const err= new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic"); //This lets the client know that the server is requesting authentication and the authentication method being requested is Basic 
+      err.status= 401; //Error code when authentication is not given
+      return next(err); //Server will send the error message back and ask for authentication from the client
+    }
+    //If the client then gives their username and password, an authorization header will then be submitted to the server
+    //Buffer global class in Node is one of the few classes in Node that we can just use.
+    //Buffer.from() just decodes the username and password from Base-64 encoded string
+    //Argument inside of Buffer.from: It takes the authorization header and extract the username and password from it. Then it places it in the auth array as username and password.
+    const auth= Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+    const user= auth[0]; //Getting the client's username from the auth array
+    const pass = auth[1]; //Getting the client's password from the auth array
+    
+    //Basic Validation 
+    if(user ==="admin" && pass ==="password") {
+      //Setting up a cookie when the username and password is correct
+      //res.cookie is part of Express response object API. res.cookie is used to create a new cookie by passing it the name we want to use in the cookie.
+      //The first argument in res.cookie is the name we want to use for the cookie, in this case it is "user"
+      //Second argument is value to store in the name property.
+      //Third argumnet is optional and contains configuration values. signed: true means we let Express know to use the secret key from Cookie Parser to create a signed cookie.
+        res.cookie("user", "admin", {signed: true});
+        return next(); //The user is authorized to use the server
+    } else { //An error shows if the user doesn't type admin and password for the username and password
+        const err = new Error("You are not authenticated!"); //Goes to the Express error handler
+        res.setHeader("WWW-Authenticate", "Basic");
+        err.status= 401;
+        return next(err);
+    }
+  } else { //If there is a signed cookie in the incoming request
+    if(req.signedCookies.user ==="admin") { //If this is true, grant access to the client to the next middleware function
+      return next();//Going to the next middleware function
+    } else {
+        const err = new Error("You are not authenticated!"); //Goes to the Express error handler
+        err.status= 401;
+        return next(err);
+    }
   }
 }
 app.use(auth); //Lets the auth middleware function run before the Express.static() middleware function. auth function makes sure that the user has inputted their credentials (authetnication)
